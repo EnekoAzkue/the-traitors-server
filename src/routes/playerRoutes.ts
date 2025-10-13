195434ca91f99ba3f5d227cae7b1f51a39487958
EnekoAzkue <eneko.azkue@ikasle.aeg.eus>
2025-10-13 13:42:03 +0200

    feat: add playerRoutes

---
+++ src/routes/playerRoutes.ts
@@ -0,0 +1,48 @@
+import express, { Router } from "express";
+const router = express.Router();
+
+import middleware from "../middlewares/verifyData";
+import playerController from "../controllers/playerController";
+
+// TESTISNG
+router.get(
+  "/kaotika",
+  middleware.verifyIdToken,       
+  playerController.getKaotikaPlayer
+);
+
+// TESTING
+router.get(
+  "/mongo",
+  middleware.verifyIdToken,       
+  playerController.getMongoPlayer
+);
+
+router.post(
+  "/log-in",
+  middleware.verifyIdToken,       
+  playerController.loginPlayer
+);
+
+router.post(
+  "/access-logged-in",
+  middleware.verifyIdToken,
+  playerController.loggedPlayer
+);
+
+router.get(
+  "/get/:playerEmail",
+  playerController.getPlayer
+);
+
+router.patch(
+  "/update/:playerEmail",
+  playerController.updatePlayer
+)
+
+router.get(
+  "/get-acolytes",
+  playerController.getAcolytes
+)
+
+export default router;
