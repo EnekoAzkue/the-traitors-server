f50cd5d008b088260d2a2c0add68046e08ea9884
EnekoAzkue <eneko.azkue@ikasle.aeg.eus>
2025-11-06 12:02:16 +0100

    merge branch 'develop' into staging

--- src/middlewares/verifyData.ts
+++ src/middlewares/verifyData.ts
@@ -8,7 +8,7 @@ async function verifyIdToken(req: any, res: any, next: any) {
     {
       method: "POST",
       body: JSON.stringify({
-        requestUri: `http://10.70.0.41:3000${req.originalUrl}`,
+        requestUri: `https://the-traitors-server.onrender.com${req.originalUrl}`,
         postBody: `id_token=${idToken}&providerId=google.com`,
         returnSecureToken: true,
       }),
