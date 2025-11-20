147cacace1624d2976e31f12ad3b8fb49e9ea094
Ignacio <ignayh2000@gmail.com>
2025-11-20 21:00:14 +0100

    fix(THET-136): Fix socket events in server to found scroll

--- src/helpers/utilities/socket/socketUtilities.ts
+++ src/helpers/utilities/socket/socketUtilities.ts
@@ -133,9 +133,9 @@ const manageMortimerNotificationEvent = (socket: Socket) => {
             });
     });
 
-    socket.on(SocketEvents.FOUND_SCROLL, () => {
+    socket.on(SocketEvents.SEND_FOUND_SCROLL, () => {
         console.log("Sending notification to Mortimer about found scroll...");
-        socket.emit(SocketEvents.FOUND_SCROLL, () => {});
+        socket.emit(SocketEvents.RECIEVED_FOUND_SCROLL, () => {});
     });
 
     socket.on(SocketEvents.SCROLL_VANISH, (message: any) => {
