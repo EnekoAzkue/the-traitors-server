3dca659129daacbcbb5f699b9d425ef202abb0da
Ignacio <ignayh2000@gmail.com>
2026-01-14 09:00:16 +0100

    fix(THET-207): When users resistance drops to 0 increase one last tiem insanity.

--- src/helpers/utilities/cron/cronUtilities.ts
+++ src/helpers/utilities/cron/cronUtilities.ts
@@ -7,15 +7,16 @@ import playerService from "../../../services/playerServices";
 // --- CRON TASKS --- //
 
 async function reducePlayerResistance(player: KaotikaUser, resistanceValue: number){
-  await playerService.updatePlayer(player.email, { resistance: player.resistance - resistanceValue });
+  return await playerService.updatePlayer(player.email, { resistance: player.resistance - resistanceValue });
 }
 
 async function increaseInsanityBasedOnResistance(player: KaotikaUser, insanityValue: number){
   if( player.resistance < DARK_HEARTBEAT.RESISTANCE_VALUE_TO_START_INCREASING_INSANITY){
     const attributesWithMoreInsanity = player.attributes;
     attributesWithMoreInsanity.insanity += insanityValue; 
-    await playerService.updatePlayer(player.email, {attributes : attributesWithMoreInsanity});
+    return await playerService.updatePlayer(player.email, {attributes : attributesWithMoreInsanity});
   }
+  return player;
 }
 
 async function getRandomDisease(){
@@ -24,14 +25,14 @@ async function getRandomDisease(){
 
 async function modifyPlayerAttributes (loyalAcolyte: KaotikaUser) {
   if (loyalAcolyte.resistance > 0) {
-    await reducePlayerResistance(loyalAcolyte, DARK_HEARTBEAT.MODIFICATION_VALUE);
-    await increaseInsanityBasedOnResistance(loyalAcolyte, DARK_HEARTBEAT.MODIFICATION_VALUE);
+    const loyalWithLessResistance = await reducePlayerResistance(loyalAcolyte, DARK_HEARTBEAT.MODIFICATION_VALUE);
+    await increaseInsanityBasedOnResistance(loyalWithLessResistance, DARK_HEARTBEAT.MODIFICATION_VALUE);
   }
 }
 
 async function executeDarkHeartbeat(){
   const loyalAcolytes = await playerService.getLoyalAcolytes();
-  loyalAcolytes.map(async (loyalAcolyte, i) => {
+  loyalAcolytes.map(async (loyalAcolyte) => {
     await modifyPlayerAttributes(loyalAcolyte);
     
     console.log(`Player: ${loyalAcolyte.name}, Resistance: ${loyalAcolyte.resistance}, Insanity: ${loyalAcolyte.attributes.insanity}`);
